e435068cac5734c9cb7c1545cb3f1b2cde9be6d6
Shinemi <simon.lucas.83136@gmail.com>
2026-03-17 14:01:25 +0100

    refactor: clean up App component and add new files for weather functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <h1>Weather Dashboard built with REACT.JS</h1>
+    <>
+      <h1>Weather Dashboard built with REACT.JS</h1>
+    </>
+    
   )
 }
 
