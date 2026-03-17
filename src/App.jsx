ed2cfd24b0ce86b6400f7a9ddd9a38df1c7d8de7
Shinemi <simon.lucas.83136@gmail.com>
2026-03-17 13:35:50 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <h1>Weather Dashboard built with REACT.JS</h1>
+  )
+}
+
+export default App
